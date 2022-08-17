06796e5b2eae76fb75f1dd9223a68bc0cf681453
BuildTools <unconfigured@null.spigotmc.org>
2022-08-17 00:41:13 +0000

    gobus

--- test.cpp
+++ test.cpp
@@ -6,7 +6,11 @@ using namespace std;
 int main() {
   int cars[10] = {4, 3, 5, 1, 6, 7, 8, 5, 4, 3};
 
-  cout << ":)" << endl;
+  for (int i = 0; i <= 4; i++) {
+    cout << i + 1 << endl;
+    if (i == 4) cout << "6, 7, 8 dollas in my bank account\n";
+
+  }
   
   return 0;
 }
